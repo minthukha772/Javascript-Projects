b2f11e842429d565666ad1ed567c15d2dba0a114
minthukha772 <94103142+minthukha772@users.noreply.github.com>
2021-12-10 02:11:08 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -1,157 +1,141 @@
-let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
+document.querySelector('#menu-bar').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
-document.querySelector('#login-btn').onclick = () =>{
-    document.querySelector('.login-form-container').classList.toggle('active');
-}
-
-document.querySelector('#close-login-form').onclick = () =>{
-    document.querySelector('.login-form-container').classList.remove('active');
-}
-
-
-
-
-window.scroll = () =>{
-
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    }
-
-
-    menu.classList.remove('fa-times');
+document.querySelector('#close').onclick = () =>{
     navbar.classList.remove('active');
 }
 
 
 
+window.onscroll = () =>{
 
-window.onload = () =>{
-
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
+    if(window.scrollY > 100){
+        document.querySelector('header').classList.add('active');
     }else{
-        document.querySelector('.header').classList.remove('active');
+        document.querySelector('header').classList.remove('active');
     }
 }
 
 
-document.querySelector('.home').onmousemove = (e) =>{
+let themeToggler = document.querySelector('#theme-toggler');
 
-    document.querySelectorAll('.home-parallax').forEach(elm =>{
-
-        let speed = elm.getAttribute('data-speed');
-
-        let x = (window.innerWidth - e.pageX * speed) / 90;
-        let y = (window.innerHeight - e.pageY * speed) / 90;
-
-        elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
-
-    });
-}
-
-document.querySelector('.home').onmouseleave = () =>{
-
-    document.querySelectorAll('.home-parallax').forEach(elm =>{
-
-        elm.style.transform = `translateX(0px) translateY(0px)`;
-    });
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.querySelector('body').classList.add('active');
+    }else{
+        document.querySelector('body').classList.remove('active');
+    }
 }
 
 
-var swiper = new Swiper(".vehicles-slider", {
-    slidesPerView:1,
-    spaceBetween:20,
-    loop:true,
-    grapCursor:true,
-    centeredSlides:true,
-    autoplay: {
-      delay: 9500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable:true,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-    },
-  });
+document.querySelectorAll('.small-image-1').forEach(images=>{
+    images.onclick = () =>{
+        document.querySelector('.big-image-1').src = images.getAttribute('src');
+    }
+});
 
 
-  
-var swiper = new Swiper(".featured-slider", {
-    slidesPerView:1,
-    spaceBetween:20,
-    loop:true,
-    grapCursor:true,
-    centeredSlides:true,
-    autoplay: {
-      delay: 9500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable:true,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      991: {
-        slidesPerView: 3,
-      },
-    },
-  });
-
-
-
-  
-  
-var swiper = new Swiper(".review-slider", {
-  slidesPerView:1,
-  spaceBetween:20,
-  loop:true,
-  grapCursor:true,
-  centeredSlides:true,
-  autoplay: {
-    delay: 9500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable:true,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    991: {
-      slidesPerView: 3,
-    },
-  },
+document.querySelectorAll('.small-image-2').forEach(images=>{
+    images.onclick = () =>{
+        document.querySelector('.big-image-2').src = images.getAttribute('src');
+    }
 });
 
 
 
+document.querySelectorAll('.small-image-3').forEach(images=>{
+    images.onclick = () =>{
+        document.querySelector('.big-image-3').src = images.getAttribute('src');
+    }
+});
+
+
+let countDate = new Date('sep, 2021 00:00:00').getTime();
+
+
+function countDown(){
+
+    let now = new Date().getTime();
+    gap = now - countDate;
+
+    let seconds = 1000;
+    let minutes = seconds*60;
+    let hours = minutes*60;
+    let day = hours * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hours));
+    let m = Math.floor((gap % (hours)) / (minutes));
+    let s = Math.floor((gap % (minutes)) / (seconds));
+
+
+    document.getElementById('days').innerText = d;
+    document.getElementById('hours').innerText = h;
+    document.getElementById('minutes').innerText = m;
+    document.getElementById('seconds').innerText = s;
+}
+
+setInterval(function(){
+    countDown()
+},1000);
+
+
+
+var swiper = new Swiper(".product-slider", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        550: {
+          slidesPerView: 2,
+        },
+        800: {
+          slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
+
+
+var swiper = new Swiper(".review-slider", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        550: {
+          slidesPerView: 2,
+        },
+        800: {
+          slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
